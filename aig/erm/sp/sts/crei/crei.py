190d014e20b3296f19f8e682863b729a0dfcbac6
Si Yu <siyu798@gmail.com>
2015-02-13 17:06:58 -0500

    updated crei

--- aig/erm/sp/sts/crei/crei.py
+++ aig/erm/sp/sts/crei/crei.py
@@ -2,17 +2,21 @@ __author__ = 'siy'
 
 import aig.erm.sp.db as db
 import itertools as it
+import csv
 
-forecast_version = '2014Q2'
-us_scen = 'Stress_98pct'
-us_weight_apt = 0.15
-us_weight_office = 0.34
-us_weight_retail = 0.44
-us_weight_warehouse = 0.07
-us_blend_ticker = 'EC.USD.CRE.INDEX.BLEND.PPR'
-euro_blend_ticker = 'EC.EUR.CRE.INDEX.BLEND.PPR'
-euro_scen = 'price_99_8_th'
-num_periods = 19
+FORECAST_VERSION = '2014Q2'
+NUM_PERIODS = 19
+
+US_SCEN = 'Stress_98pct'
+US_WEIGHT_APT = 0.15
+US_WEIGHT_OFFICE = 0.34
+US_WEIGHT_RETAIL = 0.44
+US_WEIGHT_WAREHOUSE = 0.07
+US_BLEND_TICKER = 'EC.USD.CRE.INDEX.BLEND.PPR'
+
+EURO_BLEND_TICKER = 'EC.EUR.CRE.INDEX.BLEND.PPR'
+EURO_SCEN = 'price_99_8_th'
+
 
 us_msa_sql_template = r'''
 SELECT c1.forecast_period, AVG (c1.NOI / c2.CR) AS price
@@ -171,21 +175,35 @@ us_scen_map = [{'msa': "('New York', 'Philadelphia', 'Wilmington')", 'prop_type'
                 'ticker': 'EC.USD.CRE.INDEX.Warehouse.Chattanooga.PPR'},
                {'msa': "('Fort Wayne')", 'prop_type': 'Warehouse',
                 'ticker': 'EC.USD.CRE.INDEX.Warehouse.FortWayne.PPR'},
-               {'msa': "('San Diego')", 'prop_type': 'Warehouse', 'ticker': 'EC.USD.CRE.INDEX.Warehouse.SanDiego.PPR'}]
+               {'msa': "('San Diego')", 'prop_type': 'Warehouse', 'ticker': 'EC.USD.CRE.INDEX.Warehouse.SanDiego.PPR'},
+
+               {'msa': "('New York')", 'prop_type': 'Hotel', 'ticker': 'EC.USD.CRE.INDEX.HOTEL.NewYork.PPR'},
+               {'msa': "('New York')", 'prop_type': 'Hotel', 'ticker': 'EC.USD.CRE.INDEX.HOTEL.WashNoVaMd.PPR'},
+               {'msa': "('New York')", 'prop_type': 'Hotel', 'ticker': 'EC.USD.CRE.INDEX.HOTEL.SanDiego.PPR'},
+               {'msa': "('New York')", 'prop_type': 'Hotel', 'ticker': 'EC.USD.CRE.INDEX.HOTEL.Seattle.PPR'},
+               {'msa': "('Akron', 'Grand Rapids')", 'prop_type': 'Retail',
+                'ticker': 'EC.USD.CRE.INDEX.RETAIL.Akron_GrandRapids.PPR'},
+               {'msa': "('Houston')", 'prop_type': 'Apartment', 'ticker': 'EC.USD.CRE.INDEX.OFFICE.Houston.PPR'},
+               {'msa': "('PPR54')", 'prop_type': 'Office', 'ticker': 'EC.USD.CRE.INDEX.OFFICE.Houston.PPR'},
+               {'msa': "('Burlington')", 'prop_type': 'Retail', 'ticker': 'EC.USD.CRE.INDEX.RETAIL.Burlington.PPR'},
+               {'msa': "('PPR54')", 'prop_type': 'Office', 'ticker': 'EC.USD.CRE.INDEX.OFFICE.PPR'}, ]
+
 
 euro_msa_map = [
     {'msa': "('Warsaw','Moscow')", 'prop_type': 'Office', 'ticker': 'EC.RUB_PLN.CRE.INDEX.OFFICE.MOSCOW_WARSAW.PPR'},
     {'msa': "('Moscow')", 'prop_type': 'Office', 'ticker': 'EC.RUB.CRE.INDEX.OFFICE.MOSCOW.PPR'},
     {'msa': "('Warsaw')", 'prop_type': 'Office', 'ticker': 'EC.PLN.CRE.INDEX.OFFICE.WARSAW.PPR'},
     {'msa': "('PPR Europe')", 'prop_type': 'Warehouse', 'ticker': 'EC.EUR.CRE.INDEX.WAREHOUSE.PPR'},
-    {'msa': "('PPR Europe')", 'prop_type': 'Retail', 'ticker': 'EC.EUR.CRE.INDEX.RETAIL.PPR'}, ]
+    {'msa': "('PPR Europe')", 'prop_type': 'Retail', 'ticker': 'EC.EUR.CRE.INDEX.RETAIL.PPR'},
+    {'msa': "('PPR Europe')", 'prop_type': 'Office', 'ticker': 'EC.EUR.CRE.INDEX.OFFICE.PPR'},
+    {'msa': "('Warsaw')", 'prop_type': 'Warehouse', 'ticker': 'EC.PLN.CRE.INDEX.WAREHOUSE.WARSAW.PPR'}, ]
 
 
-# query the db for result and limit the first num_records of results
+# query the db for result and limit the first num_records of results and add periods (index)
 def get_result(db_conn, sql, num_records):
     result = db.exec(db_conn, sql)
-    # take the first <num_records> of elements
-    return list(it.islice(result, num_records))
+    # take the first <num_records> of elements and add period
+    return [dict(r, **{'PERIOD': idx}) for idx, r in enumerate(result[:num_records])]
 
 
 db_conn = db.get_conn('idwdev', 'erm_riskagg', 'aig_1_amg')
@@ -193,33 +211,33 @@ output = []
 
 # US MSA
 for m in us_scen_map:
-    sql = us_msa_sql_template.format(scenario=us_scen, msa=m['msa'],
-                                     forecast_version=forecast_version, prop_type=m['prop_type'])
-    rs = get_result(db_conn, sql, num_periods)
+    sql = us_msa_sql_template.format(scenario=US_SCEN, msa=m['msa'],
+                                     forecast_version=FORECAST_VERSION, prop_type=m['prop_type'])
+    rs = get_result(db_conn, sql, NUM_PERIODS)
     rs = [dict(r, **{'TICKER': m['ticker']}) for r in rs]
     output.append(rs)
 
 # US Blend
-us_blend_sql = us_blend_sql_template.format(scenario=us_scen, forecast_version=forecast_version,
-                                            weight_apt=us_weight_apt, weight_office=us_weight_apt,
-                                            weight_retail=us_weight_retail, weight_warehouse=us_weight_warehouse)
-us_blend_res = get_result(db_conn, us_blend_sql, num_periods)
-us_blend_res = [dict(r, **{'TICKER': us_blend_ticker}) for r in us_blend_res]
+us_blend_sql = us_blend_sql_template.format(scenario=US_SCEN, forecast_version=FORECAST_VERSION,
+                                            weight_apt=US_WEIGHT_APT, weight_office=US_WEIGHT_OFFICE,
+                                            weight_retail=US_WEIGHT_RETAIL, weight_warehouse=US_WEIGHT_WAREHOUSE)
+us_blend_res = get_result(db_conn, us_blend_sql, NUM_PERIODS)
+us_blend_res = [dict(r, **{'TICKER': US_BLEND_TICKER}) for r in us_blend_res]
 output.append(us_blend_res)
 
 # Euro MSA
 for m in euro_msa_map:
-    sql = euro_msa_sql_template.format(scenario=euro_scen, msa=m['msa'],
-                                       forecast_version=forecast_version, prop_type=m['prop_type'])
-    rs = get_result(db_conn, sql, num_periods)
+    sql = euro_msa_sql_template.format(scenario=EURO_SCEN, msa=m['msa'],
+                                       forecast_version=FORECAST_VERSION, prop_type=m['prop_type'])
+    rs = get_result(db_conn, sql, NUM_PERIODS)
     rs = [dict(r, **{'TICKER': m['ticker'], 'PRICE': float(r['PRICE']) / float(rs[0]['PRICE'])}) for r in rs]
     output.append(rs)
 
 # Euro Blend
-euro_blend_sql = euro_blend_sql_template.format(forecast_version=forecast_version)
-euro_blend_res = get_result(db_conn, euro_blend_sql, num_periods)
+euro_blend_sql = euro_blend_sql_template.format(forecast_version=FORECAST_VERSION)
+euro_blend_res = get_result(db_conn, euro_blend_sql, NUM_PERIODS)
 euro_blend_res = [
-    dict(r, **{'TICKER': euro_blend_ticker, 'PRICE': float(r['PRICE']) / float(euro_blend_res[0]['PRICE'])}) for r in
+    dict(r, **{'TICKER': EURO_BLEND_TICKER, 'PRICE': float(r['PRICE']) / float(euro_blend_res[0]['PRICE'])}) for r in
     euro_blend_res]
 output.append(euro_blend_res)
 
@@ -227,3 +245,13 @@ db_conn.close()
 
 for i in output:
     print(i)
+
+with open('crei_sts.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, ['TICKER', 'PRICE', 'PERIOD'])
+    writer.writeheader()
+    for ticker in output:
+        for period in ticker:
+            csv_row = {'TICKER': period['TICKER'],
+                       'PRICE': period['PRICE'],
+                       'PERIOD': period['PERIOD']}
+            writer.writerow(csv_row)
--- aig/erm/sp/sts/ushpi/ushpi.py
+++ aig/erm/sp/sts/ushpi/ushpi.py
@@ -2,99 +2,108 @@ __author__ = 'siy'
 
 import csv
 import itertools as it
-
-print("Start generating US HPI STS csv...")
-
-# load scenario data
-scen = []
-with open('scenario_us_hpi.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        scen.append(row)
-
-# group the data by CBSA Id and keep the third months in period column
-scen_grouped = {}
-for k, g in it.groupby(scen, lambda d: d.get('CBSA')):
-    # keep the every third month
-    t = [r for r in g if (int(r.get('Period')) + 1) % 3 == 0]
-    # give each qtr an index
-    scen_grouped[k] = [dict(r, **{'Period_in_qtr': int((int(r.get('Period')) + 1) / 3 - 1)}) for r in t]
-
-# for k, g in scen_grouped.items():
-# print(k, g)
-print('number of scenario msa: ', len(scen_grouped))
-
-# load historical data
-hist = []
-with open('us_hpi_hist_2014q4.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        hist.append(row)
-
-# group the hist data and order by period in reverse order. ie latest price comes first
-hist_grouped = {}
-for k, g in it.groupby(hist, lambda d: d.get('state')):
-    hist_grouped[k] = sorted(g, key=lambda r: r.get('time'), reverse=True)
-
-# for k, g in hist_grouped.items():
-# print(k, g)
-print('number of historical msa: ', len(hist_grouped))
+import datetime as dt
 
 
-# # merge hist and scenario
-scen_hist_merged = {}
-for cbsa, scen in scen_grouped.items():
+def gen_sts_us_hpi(scen_hpi_csv, hist_hpi_csv, result_csv, subject_area):
 
-    hist = hist_grouped.get(cbsa)
-    if not hist: continue  # no historical data
+    print("Start generating US HPI STS csv...")
+    as_of_at_date = dt.datetime.now().strftime("%m/%d/%Y");
 
-    t0 = hist[0]
-    if not t0.get('ticker'): continue  # no ticker
+    # load scenario data
+    scen = []
+    with open(scen_hpi_csv) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            scen.append(row)
 
-    cbsa_merged = []
-    # add scenario periods
-    for r in scen:
-        d = {'cbsa_code': cbsa, 'cbsa_name': t0.get('ticker'),
-             'period': r.get('Period_in_qtr'), 'hpi': (float(r.get('HPI')) * float(t0.get('hpiclst')))}
-        cbsa_merged.append(d)
+    # group the data by CBSA Id and keep the third months in period column
+    scen_grouped = {}
+    for k, g in it.groupby(scen, lambda d: d.get('CBSA')):
+        # keep the every third month to get quarterly hpi
+        t = [r for r in g if int(r.get('Period')) % 3 == 0]
+        # give each qtr an index
+        scen_grouped[k] = [dict(r, **{'Period_in_qtr': (int(r.get('Period')) / 3)}) for r in t]
 
-    # add historical periods
-    for idx, r in enumerate(hist):
-        if idx == 0: continue  # skip t0
-        d = {'cbsa_code': cbsa, 'cbsa_name': r.get('ticker'),
-             'period': -idx, 'hpi': r.get('hpiclst')}
-        cbsa_merged.append(d)
+    # for k, g in scen_grouped.items():
+    # print(k, g)
+    print('number of scenario msa: ', len(scen_grouped))
 
-    # sort by periods
-    scen_hist_merged[cbsa] = sorted(cbsa_merged, key=lambda r: r.get('period'))
+    # load historical data
+    hist = []
+    with open(hist_hpi_csv) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            hist.append(row)
 
-# for r in scen_hist_merged['0']:
-# print(r)
+    # group the hist data and order by period in reverse order. ie latest price comes first
+    hist_grouped = {}
+    for k, g in it.groupby(hist, lambda d: d.get('state')):
+        hist_grouped[k] = sorted(g, key=lambda r: r.get('time'), reverse=True)
+
+    # for k, g in hist_grouped.items():
+    # print(k, g)
+    print('number of historical msa: ', len(hist_grouped))
 
 
-# write to csv
-with open('us_hpi_result.csv', 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, ['SCENARIO_AS_OF_DATE', 'SCENARIO_AS_AT_DATE', 'SUBJECT_AREA',
-                                      'PARAMETER_TYPE', 'PARAMETER_NAME', 'SCENARIO_PARA_VAL_TYPE',
-                                      'SCENARIO_TYPE_CODE', 'UNITS', 'DURATION_KEY',
-                                      'SCENARIO_PERIOD_NUMBER', 'SCENARIO_DATATYPE_NAME', 'SCENARIO_PARA_VAL_NAME',
-                                      'SCENARIO_PARAMETER_VALUE'])
-    writer.writeheader()
-    for cbsa, periods in scen_hist_merged.items():
-        for p in periods:
-            csv_row = {'SCENARIO_AS_OF_DATE': '12/31/2014',
-                       'SCENARIO_AS_AT_DATE': '2/3/2015',
-                       'SUBJECT_AREA': 'AGE HPI Corelogic',
-                       'PARAMETER_TYPE': 'EC',
-                       'PARAMETER_NAME': p.get('cbsa_name'),
-                       'SCENARIO_PARA_VAL_TYPE': 'Absolute',
-                       'SCENARIO_TYPE_CODE': '2015RC_Stress99p5',
-                       'UNITS': 'Whole Number',
-                       'DURATION_KEY': 'Q',
-                       'SCENARIO_PERIOD_NUMBER': p.get('period'),
-                       'SCENARIO_DATATYPE_NAME': 'Numeric',
-                       'SCENARIO_PARA_VAL_NAME': 'Scenario',
-                       'SCENARIO_PARAMETER_VALUE': p.get('hpi')}
-            writer.writerow(csv_row)
+    # # merge hist and scenario
+    scen_hist_merged = {}
+    for cbsa, scen in scen_grouped.items():
 
-print("Finished generating US HPI STS csv...")
+        hist = hist_grouped.get(cbsa)
+        if not hist: continue  # no historical data
+
+        t0 = hist[0]
+        if not t0.get('ticker'): continue  # no ticker
+
+        cbsa_merged = []
+        # add scenario periods
+        for r in scen:
+            d = {'cbsa_code': cbsa, 'cbsa_name': t0.get('ticker'),
+                 'period': r.get('Period_in_qtr'), 'hpi': (float(r.get('HPI')) * float(t0.get('hpiclst')))}
+            cbsa_merged.append(d)
+
+        # add historical periods
+        for idx, r in enumerate(hist):
+            if idx == 0: continue  # skip t0
+            d = {'cbsa_code': cbsa, 'cbsa_name': r.get('ticker'),
+                 'period': -idx, 'hpi': r.get('hpiclst')}
+            cbsa_merged.append(d)
+
+        # sort by periods
+        scen_hist_merged[cbsa] = sorted(cbsa_merged, key=lambda r: r.get('period'))
+
+    # for r in scen_hist_merged['0']:
+    # print(r)
+
+
+    # write to csv
+    with open(result_csv, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, ['SCENARIO_AS_OF_DATE', 'SCENARIO_AS_AT_DATE', 'SUBJECT_AREA',
+                                          'PARAMETER_TYPE', 'PARAMETER_NAME', 'SCENARIO_PARA_VAL_TYPE',
+                                          'SCENARIO_TYPE_CODE', 'UNITS', 'DURATION_KEY',
+                                          'SCENARIO_PERIOD_NUMBER', 'SCENARIO_DATATYPE_NAME', 'SCENARIO_PARA_VAL_NAME',
+                                          'SCENARIO_PARAMETER_VALUE'])
+        writer.writeheader()
+        for cbsa, periods in scen_hist_merged.items():
+            for p in periods:
+                csv_row = {'SCENARIO_AS_OF_DATE': '12/31/2014',
+                           'SCENARIO_AS_AT_DATE': as_of_at_date,
+                           'SUBJECT_AREA': subject_area,
+                           'PARAMETER_TYPE': 'EC',
+                           'PARAMETER_NAME': p.get('cbsa_name'),
+                           'SCENARIO_PARA_VAL_TYPE': 'Absolute',
+                           'SCENARIO_TYPE_CODE': 'SPRC',
+                           'UNITS': 'Whole Number',
+                           'DURATION_KEY': 'Q',
+                           'SCENARIO_PERIOD_NUMBER': p.get('period'),
+                           'SCENARIO_DATATYPE_NAME': 'Numeric',
+                           'SCENARIO_PARA_VAL_NAME': 'Scenario',
+                           'SCENARIO_PARAMETER_VALUE': p.get('hpi')}
+                writer.writerow(csv_row)
+
+    print("Finished generating US HPI STS csv...")
+
+
+gen_sts_us_hpi('RC_2015Q1_995_HPI_CL_AGEBase.csv', 'us_hpi_cl_hist_2014q4.csv', 'us_hpi_cl_sts_2015q1.csv', 'AGE HPI Corelogic')
+gen_sts_us_hpi('RC_2015Q1_995_HPI_FHFA_AGEBase.csv', 'us_hpi_fhfa_hist_2014q3.csv', 'us_hpi_fhfa_sts_2015q1.csv', 'AGE HPI FHFA')
