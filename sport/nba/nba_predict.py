4852d2c93eb348fe9414f282aad767b11a62362d
Si Yu <siyu798@gmail.com>
2015-12-29 12:57:18 -0500

    nba_predict: fit by team misc stats

--- sport/nba/nba_predict.py
+++ sport/nba/nba_predict.py
@@ -4,6 +4,7 @@ import statsmodels.api as sm
 
 team_scores_url = 'http://www.basketball-reference.com/leagues/NBA_2016_games.html'
 team_stats_url = 'http://www.basketball-reference.com/leagues/NBA_2016.html'
+team_misc_url = 'nba_misc_stats.csv'
 
 
 def get_team_scores(url):
@@ -22,28 +23,61 @@ def get_team_stats(url):
     return s.iloc[:-1, :]
 
 
-scores = get_team_scores(team_scores_url)
-num_scores = len(scores)
-team_stats = get_team_stats(team_stats_url)
-teams = sorted(team_stats['Team'].values)
-num_teams = len(teams)
-scores['home-away'] = scores['PTS.1'] - scores['PTS'] - 2  # home court adv = 2 pts
-x = np.zeros([num_scores, num_teams])
+def get_team_misc_stats():
+    s = pd.read_csv(team_misc_url)
+    return s.iloc[:-1, :]
 
-for idx, row in scores.iterrows():
-    home = row['Home/Neutral']
-    away = row['Visitor/Neutral']
-    home_idx = teams.index(home)
-    away_idx = teams.index(away)
-    x[idx][home_idx] = 1
-    x[idx][away_idx] = -1
 
-x = pd.DataFrame(x, columns=teams)
-y = scores['home-away']
-model = sm.OLS(y, x)
-result = model.fit()
-print(result.summary())
+def predict_rank():
+    scores = get_team_scores(team_scores_url)
+    num_scores = len(scores)
+    team_stats = get_team_stats(team_stats_url)
+    teams = sorted(team_stats['Team'].values)
+    num_teams = len(teams)
+    scores['home-away'] = scores['PTS.1'] - scores['PTS'] - 2  # home court adv = 2 pts
+    x = np.zeros([num_scores, num_teams])
 
-# team_stats.loc[team_stats['Team']=='Golden State Warriors'].iloc[:,2:].values - team_stats.loc[team_stats['Team']=='Cleveland Cavaliers'].iloc[:,2:].values
+    for idx, row in scores.iterrows():
+        home = row['Home/Neutral']
+        away = row['Visitor/Neutral']
+        home_idx = teams.index(home)
+        away_idx = teams.index(away)
+        x[idx][home_idx] = 1
+        x[idx][away_idx] = -1
+
+    x = pd.DataFrame(x, columns=teams)
+    y = scores['home-away']
+    model = sm.OLS(y, x)
+    result = model.fit()
+    print(result.summary())
+
+
+def predict_by_stats():
+    scores = get_team_scores(team_scores_url)
+    num_scores = len(scores)
+    team_stats = get_team_misc_stats()
+    scores['home-away'] = scores['PTS.1'] - scores['PTS'] - 2  # home court adv = 2 pts
+
+    # param_columns = ['FTr',	'3PAr',	'TS%',	'eFG%',	'TOV%',	'ORB%',	'FT/FGA',	'eFG%',	'TOV%',	'DRB%',	'FT/FGA']
+    param_columns = ['FTr',	'TS%',	'eFG%',	'TOV%',	'ORB%',	'FT/FGA',	'eFG%',	'TOV%',		'FT/FGA']
+    num_params = len(param_columns)
+    x = np.zeros([num_scores, num_params])
+
+    for idx, row in scores.iterrows():
+        home = row['Home/Neutral']
+        away = row['Visitor/Neutral']
+        x[idx] = team_stats.loc[team_stats['Team'] == home][param_columns].values - \
+                 team_stats.loc[team_stats['Team'] == away][param_columns].values
+
+    x = pd.DataFrame(x, columns=param_columns)
+    y = scores['home-away']
+    model = sm.OLS(y, x)
+    result = model.fit()
+    print(result.summary())
+
+# team_stats.loc[team_stats['Team']=='Golden State Warriors'][param_columns].values - team_stats.loc[team_stats['Team']=='Cleveland Cavaliers'][param_columns].values
+
+predict_by_stats()
+predict_rank()
 
 print('done')
