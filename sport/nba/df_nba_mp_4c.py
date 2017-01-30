08913c936860451f720815af5b16cf5384caeb1f
Si Yu <siyu798@gmail.com>
2017-01-30 00:08:44 -0500

    df_nba: calc player mean and std

--- sport/nba/df_nba_mp_4c.py
+++ sport/nba/df_nba_mp_4c.py
@@ -1,32 +1,39 @@
 import pandas as pd
-import random as r
+import random as rand
 import numpy as np
 import pathos.multiprocessing as mp
+import operator
 
-from sport.nba.get_player_stats import player_stat_all, injury_list
+from sport.nba.get_player_stats import player_stat_all, injury_list, player_gamelog, player_name_norm
 
 
 # TODO:
-# + fix player on a position
+# + filter by team
+# + use vegas projected team score as multiplier
 # + pick players from team with more injured players
-# + strategy: pick players with high mean low std + players from teams with injured players
+# + report lineup with most injured team mates
+
 
 
 def df_run(ps_nba_miner,
-           player_stat_file='input/DKSalaries_20161209_9g.csv',
-           NUM_SIM=1000000,  # default 1M
-           MAX_ON=['Min', 240],
+           player_stat_file='input/DKSalaries_20170129_6g.csv',
+           NUM_SIM=500000,  # default 1M
+           MAX_ON=[operator.ge, 'Min', 230],
            MAX_COST_PER_PT_OVERRIDE=np.nan,  # default np.nan, 2015-2016:203
            MIN_AVG_PTS_PER_GAME=15,  # default 15
            MIN_MINUTE=15,  # deafult 15
-           SKIP_PLAYER_LIST=[]):
-
+           SKIP_PLAYER_LIST=[],
+           PREFILLED_PLAYERS={'PG': None, 'SG': None, 'SF': None, 'PF': None, 'C': None, 'G': None, 'F': None,
+                              'UTIL': None}):
     SALARY_CAP = 50000
-    MIN_SALARY_USED = 49500         # default 49500
+    MIN_SALARY_USED = 40000  # default 49500
     MIN_PLAYER_SALARY = 3000
 
     def get_index_by_pos(df, pos):
         return df[df['Position'].str.contains(pos)].index
+
+    def get_index_by_name(df, name):
+        return df[df['Name'] == name].index.values
 
     def unique_list(l):
         return len(l) == len(set(l))
@@ -37,10 +44,8 @@ def df_run(ps_nba_miner,
     ps_dk_src = pd.read_csv(player_stat_file)
     print('Number of Players: {}'.format(len(ps_dk_src)))
 
-    SKIP_PLAYER_LIST = [n.replace("'", '').replace(',', '').replace('.', '') for n in SKIP_PLAYER_LIST]
-    ps_dk_src['Name'] = ps_dk_src['Name'].str.replace("'", '')
-    ps_dk_src['Name'] = ps_dk_src['Name'].str.replace(',', '')
-    ps_dk_src['Name'] = ps_dk_src['Name'].str.replace('.', '')
+    SKIP_PLAYER_LIST = [player_name_norm(n) for n in SKIP_PLAYER_LIST]
+    ps_dk_src['Name'] = ps_dk_src['Name'].str.replace("[',.-]", '')
 
     ps_dk_merged = pd.merge(ps_dk_src, ps_nba_miner, left_on='Name', right_on='Player', how='left')
 
@@ -50,42 +55,63 @@ def df_run(ps_nba_miner,
     ps_dk_tmp = ps_dk_merged[(ps_dk_merged['Salary'] >= MIN_PLAYER_SALARY) &
                              (ps_dk_merged['dk_score_calc'] >= MIN_AVG_PTS_PER_GAME) &
                              (ps_dk_merged['Min'] >= MIN_MINUTE) &
-                             (np.logical_not(ps_dk_merged['Name'].isin(SKIP_PLAYER_LIST)))]
+                             (np.logical_not(ps_dk_merged['Name'].isin(SKIP_PLAYER_LIST)))
+                             | ps_dk_merged['Name'].isin(PREFILLED_PLAYERS.values())]
 
     print('Number of Players after first filter: {}'.format(len(ps_dk_tmp)))
 
     cost_pt_mean = ps_dk_tmp['salary/dk_score_calc'].replace(np.inf, np.nan).dropna().mean()
     cost_pt_std = ps_dk_tmp['salary/dk_score_calc'].replace(np.inf, np.nan).dropna().std()
-    MAX_COST_PER_PT = cost_pt_mean - cost_pt_std / 3 if np.isnan(MAX_COST_PER_PT_OVERRIDE) else MAX_COST_PER_PT_OVERRIDE
+    # MAX_COST_PER_PT = cost_pt_mean - cost_pt_std / 6 if np.isnan(MAX_COST_PER_PT_OVERRIDE) else MAX_COST_PER_PT_OVERRIDE
+    MAX_COST_PER_PT = cost_pt_mean if np.isnan(MAX_COST_PER_PT_OVERRIDE) else MAX_COST_PER_PT_OVERRIDE
 
     print('After first filter: cost_pt_mean={}, cost_pt_std={}, MAX_COST_PER_PT={}'
           .format(cost_pt_mean, cost_pt_std, MAX_COST_PER_PT))
 
-    ps_dk = ps_dk_tmp[ps_dk_merged['salary/dk_score_calc'] <= MAX_COST_PER_PT]
-
+    ps_dk = ps_dk_tmp[
+        (ps_dk_tmp['salary/dk_score_calc'] <= MAX_COST_PER_PT) | ps_dk_tmp['Name'].isin(PREFILLED_PLAYERS.values())]
     print('Number of Players after MAX_COST_PER_PT filter: {}'.format(len(ps_dk)))
-    print('Players remain after MAX_COST_PER_PT filter:\n {}'.format(
-        ps_dk_merged.loc[ps_dk.index, ['Player', 'Salary', 'Min', 'dk_score_calc', 'salary/dk_score_calc']].values))
+
+    ps_dk = ps_dk_tmp
+
+    for i, row in ps_dk.iterrows():
+        new_stat = player_gamelog(row['Player'])
+        ps_dk.loc[i, 'score_mean'] = round(new_stat['mean'], 2)
+        ps_dk.loc[i, 'score_std'] = round(new_stat['std'], 2)
+        ps_dk.loc[i, 'score_norm'] = round(new_stat['std'] / new_stat['mean'], 2)
+
     print()
 
-    pg_index = get_index_by_pos(ps_dk, 'PG')
-    sg_index = get_index_by_pos(ps_dk, 'SG')
-    sf_index = get_index_by_pos(ps_dk, 'SF')
-    pf_index = get_index_by_pos(ps_dk, 'PF')
-    c_index = get_index_by_pos(ps_dk, 'C')
-    g_index = get_index_by_pos(ps_dk, 'G')
-    f_index = get_index_by_pos(ps_dk, 'F')
-    util_index = ps_dk.index
+    for i, row in ps_dk.iterrows():
+        print('{:20}  {:<6}  {:<6}  min={:<6}  avg_score={:<6}  std/mean={:<6}  salary/fp={:<6}'
+              .format(row['Player'],
+                      row['Position'],
+                      row['Salary'],
+                      row['Min'],
+                      row['score_mean'],
+                      row['score_norm'],
+                      row['salary/dk_score_calc']))
+
+    print()
+
+    pg_index = get_index_by_name(ps_dk_src, PREFILLED_PLAYERS.get('PG')) or get_index_by_pos(ps_dk, 'PG')
+    sg_index = get_index_by_name(ps_dk_src, PREFILLED_PLAYERS.get('SG')) or get_index_by_pos(ps_dk, 'SG')
+    sf_index = get_index_by_name(ps_dk_src, PREFILLED_PLAYERS.get('SF')) or get_index_by_pos(ps_dk, 'SF')
+    pf_index = get_index_by_name(ps_dk_src, PREFILLED_PLAYERS.get('PF')) or get_index_by_pos(ps_dk, 'PF')
+    c_index = get_index_by_name(ps_dk_src, PREFILLED_PLAYERS.get('C')) or get_index_by_pos(ps_dk, 'C')
+    g_index = get_index_by_name(ps_dk_src, PREFILLED_PLAYERS.get('G')) or get_index_by_pos(ps_dk, 'G')
+    f_index = get_index_by_name(ps_dk_src, PREFILLED_PLAYERS.get('F')) or get_index_by_pos(ps_dk, 'F')
+    util_index = get_index_by_name(ps_dk_src, PREFILLED_PLAYERS.get('UTIL')) or ps_dk.index
 
     def sim(i):
-        lineup = [r.choice(pg_index),
-                  r.choice(sg_index),
-                  r.choice(sf_index),
-                  r.choice(pf_index),
-                  r.choice(c_index),
-                  r.choice(g_index),
-                  r.choice(f_index),
-                  r.choice(util_index)]
+        lineup = [rand.choice(pg_index),
+                  rand.choice(sg_index),
+                  rand.choice(sf_index),
+                  rand.choice(pf_index),
+                  rand.choice(c_index),
+                  rand.choice(g_index),
+                  rand.choice(f_index),
+                  rand.choice(util_index)]
 
         # prevent one play in more than one position
         if not unique_list(lineup): return
@@ -97,10 +123,10 @@ def df_run(ps_nba_miner,
 
         if total_salary > SALARY_CAP or total_salary < MIN_SALARY_USED: return
 
-        total_max_on = ps_dk.loc[lineup, MAX_ON[0]].sum()
+        total_max_on = ps_dk.loc[lineup, MAX_ON[1]].sum()
 
-        if total_max_on >= MAX_ON[1]:
-            return [lineup, total_salary, total_max_on]
+        if MAX_ON[0](total_max_on, MAX_ON[2]):
+            return (lineup, total_salary, round(total_max_on, 1))
 
     pool = mp.Pool(4)
 
@@ -108,35 +134,24 @@ def df_run(ps_nba_miner,
 
     results = [x for x in results if x]
 
-    result = max(results, key=lambda a: a[2])
+    results_sorted = sorted(results, key=lambda a: a[2])
 
-    best_result = result
-
-    print(ps_dk.loc[best_result[0], ['Name', 'Min', 'dk_score_calc', 'AvgPointsPerGame', 'Salary']].sum())
-    print(ps_dk.loc[best_result[0], ['Name', 'Min', 'dk_score_calc', 'AvgPointsPerGame', 'Salary']])
-    print(ps_dk.loc[best_result[0], 'Name'].values)
-    print()
-
-    best_players = ps_dk.loc[best_result[0], 'Name'].values
-
-    return best_players
+    for r in results_sorted[-100:-1]:
+        print('{} min({:.0f}) std({:.1f})'.format(r,
+                                                  ps_dk.loc[r[0], 'Min'].sum(),
+                                                  np.sqrt(np.power(ps_dk.loc[r[0], 'score_std'], 2).sum())))
+        print(ps_dk.loc[r[0], 'Name'].values)
+        print()
 
 
-out_list = injury_list() + ['Channing Frye', 'Manu Ginobili', 'Pau Gasol']
+out_list = injury_list() + ['Channing Frye', 'Manu Ginobili', 'Ish Smith', 'Rajon Rondo']
 
 ps_nba_miner = player_stat_all()
 
-lineup_rotate_size = 3
-
-lineup_1 = df_run(ps_nba_miner, MAX_ON=['cost_eff', 1], SKIP_PLAYER_LIST=out_list)
-lineup_1 = df_run(ps_nba_miner, MAX_ON=['cost_eff', 1], SKIP_PLAYER_LIST=out_list + r.sample(lineup_1.tolist(), lineup_rotate_size))
-lineup_1 = df_run(ps_nba_miner, MAX_ON=['cost_eff', 1], SKIP_PLAYER_LIST=out_list + r.sample(lineup_1.tolist(), lineup_rotate_size))
-lineup_1 = df_run(ps_nba_miner, MAX_ON=['cost_eff', 1], SKIP_PLAYER_LIST=out_list + r.sample(lineup_1.tolist(), lineup_rotate_size))
-lineup_1 = df_run(ps_nba_miner, SKIP_PLAYER_LIST=out_list)
-lineup_1 = df_run(ps_nba_miner, SKIP_PLAYER_LIST=out_list + r.sample(lineup_1.tolist(), lineup_rotate_size))
-lineup_1 = df_run(ps_nba_miner, SKIP_PLAYER_LIST=out_list + r.sample(lineup_1.tolist(), lineup_rotate_size))
-lineup_1 = df_run(ps_nba_miner, SKIP_PLAYER_LIST=out_list + r.sample(lineup_1.tolist(), lineup_rotate_size))
-
-
-
-# lineup_1 = df_run(SKIP_PLAYER_LIST=out_list)
+df_run(ps_nba_miner,
+       #MAX_ON=[operator.le, 'score_norm', 100],
+       MAX_ON=[operator.ge, 'dk_score_calc', 220],
+       SKIP_PLAYER_LIST=out_list,
+       # PREFILLED_PLAYERS={'PG': None, 'SG': None, 'SF': None, 'PF': None, 'C': 'Zaza Pachulia', 'G': None, 'F': None, 'UTIL': None}
+       PREFILLED_PLAYERS={'PG': None, 'SG': None, 'SF': None, 'PF': None, 'C': None, 'G': None, 'F': None, 'UTIL': None}
+       )
--- sport/nba/get_player_stats.py
+++ sport/nba/get_player_stats.py
@@ -31,9 +31,7 @@ def load_player_stat(url):
     ps_df = ps_df.reindex(ps_df.index.drop(0))
 
     # remove special characters in player name
-    ps_df['Player'] = ps_df['Player'].str.replace("'", '')
-    ps_df['Player'] = ps_df['Player'].str.replace(',', '')
-    ps_df['Player'] = ps_df['Player'].str.replace('.', '')
+    ps_df['Player'] = ps_df['Player'].str.replace("[',.-]", '')
 
     # normalize names
     ps_df['Player'] = ps_df['Player'].str.replace('Jose Juan Barea', 'JJ Barea')
@@ -45,25 +43,6 @@ def load_player_stat(url):
     ps_df['Player'] = ps_df['Player'].str.replace('Luc Mbah a Moute', 'Luc Richard Mbah a Moute')
 
     return ps_df
-    # pt = 1
-    # 3pt = 0.5
-    # rebound = 1.25
-    # assist = 1.5
-    # steal = 2
-    # block = 2
-    # turnover = -0.5
-    # double-double = 1.5
-    # triple-double = 3
-
-    # team_stats_df['DK_Score'] = team_stats_df['PTS'] + \
-    #                             team_stats_df['3P']*0.5 + \
-    #                             team_stats_df['TRB']*1.25 + \
-    #                             team_stats_df['AST']*1.5 + \
-    #                             team_stats_df['STL']*2 + \
-    #                             team_stats_df['BLK']*2 + \
-    #                             team_stats_df['TOV']*-0.5
-    #
-    #
 
 
 def player_stat_all():
@@ -88,6 +67,9 @@ def player_stat_all():
 
     player_stat_df['dk_score_calc'] = np.round(player_stat_df['dk_score_calc'], 1)
 
+    player_stat_df = player_stat_df.loc[player_stat_df['dk_score_calc'] != player_stat_df['dk_score_calc'].min()]
+    player_stat_df = player_stat_df.loc[player_stat_df['dk_score_calc'] != player_stat_df['dk_score_calc'].max()]
+
     player_stat_df['Min'] = player_stat_df['Min'].astype(float)
     player_stat_df['pts/min'] = player_stat_df['dk_score_calc'] / player_stat_df['Min']
 
@@ -104,15 +86,54 @@ def injury_list():
     t = t.reindex(t.index.drop(1))
     players = t['Player'].values.tolist()
 
-    return [p.replace("'", '').replace(',', '').replace('.', '') for p in players]
+    return [player_name_norm(p) for p in players]
 
 
-def player_gamelog():
-    p = pd.read_html('http://www.basketball-reference.com/players/w/westbru01/gamelog/2017/')[7]
-    p = pd.read_html('http://www.basketball-reference.com/players/w/aldrila01/gamelog/2017/')[7]
+def player_name_norm(player_name):
+    return player_name.replace("'", '').replace(',', '').replace('.', '').replace('-', '')
+
+
+def player_gamelog(player_name):
+    name_codes = {'Marcus Williams': '04',
+                  'Marvin Williams': '02',
+                  'Kemba Walker': '02',
+                  'Bojan Bogdanovic': '02',
+                  'Harrison Barnes': '02',
+                  'Gerald Henderson': '02',
+                  'Anthony Davis': '02',
+                  'Marcus Morris': '03',
+                  'Matt Barnes': '02',
+                  'Brandon Knight': '03',
+                  'Wesley Matthews': '02',
+                  'Lou Williams': '02',
+                  'Isaiah Thomas': '02',
+                  'Tobias Harris': '02',
+                  'Markieff Morris': '02',
+                  'David Lee': '02',
+                  'Andre Roberson': '03',
+                  'Joe Johnson': '02',
+                  'Jeff Green': '02',
+                  'Danny Green': '02'}
+    code = name_codes.get(player_name, '01')
+    p_names = player_name.split(' ')
+    name_in_url = p_names[len(p_names) - 1][:5] + p_names[0][:2] + code
+
+    if player_name == 'Clint Capela':
+        name_in_url = 'capelca01'
+    elif player_name == 'Larry Nance Jr':
+        name_in_url = 'nancela02'
+    elif player_name == 'JJ Barea':
+        name_in_url = 'bareajo01'
+    elif player_name == 'Tim Hardaway Jr':
+        name_in_url = 'hardati02'
+
+    # p = pd.read_html('http://www.basketball-reference.com/players/w/westbru01/gamelog/2017/')[7]
+    url = 'http://www.basketball-reference.com/players/w/{}/gamelog/2017/'.format(name_in_url)
+    print(player_name, url)
+    p = pd.read_html(url)[7]
 
     # remove extra rows
-    p = p[p['GS'] == '1']
+    p = p[(p['GS'].astype(str) == '1') | (p['GS'].astype(str) == '0')]
 
     p['dk_score_calc'] = p['PTS'].astype(float) + \
                          p['3P'].astype(float) * 0.5 + \
@@ -122,6 +143,65 @@ def player_gamelog():
                          p['BLK'].astype(float) * 2 + \
                          p['TOV'].astype(float) * -0.5
 
-    return {'mean': p['dk_score_calc'].mean(), 'std': p['dk_score_calc'].std()}
+    for i, r in p.iterrows():
+        doubles = sum([int(i) for i in [float(r['PTS']) >= 10,
+                                        float(r['TRB']) >= 10,
+                                        float(r['AST']) >= 10,
+                                        float(r['STL']) >= 10,
+                                        float(r['BLK']) >= 10]])
+        if doubles > 2:
+            p.loc[i, 'dk_score_calc'] += 4.5
+        elif doubles > 1:
+            p.loc[i, 'dk_score_calc'] += 1.5
 
-    # TODO: add double-double and triple-double
+    # remove one min and one max to lessen outlier effect
+    p = p.loc[p['dk_score_calc'] != p['dk_score_calc'].min()]
+    p = p.loc[p['dk_score_calc'] != p['dk_score_calc'].max()]
+
+    return {'mean': p['dk_score_calc'].mean(), 'std': p['dk_score_calc'].std(), 'hist': p}
+
+
+def get_team_stat():
+    # PhantomJS files have different extensions
+    # under different operating systems
+    if platform.system() == 'Windows':
+        PHANTOMJS_PATH = './phantomjs.exe'
+    else:
+        PHANTOMJS_PATH = './phantomjs'
+
+    # here we'll use pseudo browser PhantomJS,
+    # but browser can be replaced with browser = webdriver.FireFox(),
+    # which is good for debugging.
+    browser = webdriver.PhantomJS(PHANTOMJS_PATH)
+    # browser.get('http://www.basketball-reference.com/leagues/NBA_2017.html#team-stats-base::none')
+    browser.get('http://www.basketball-reference.com/leagues/NBA_2017.html')
+
+    # let's parse our html
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    # get all the games
+    soup_team_stats = \
+        soup.find('div', id='all_team-stats-per_game').find_all(text=lambda text: isinstance(text, Comment))[0]
+    team_stats_df = pd.read_html(soup_team_stats.__str__())[0]
+    return team_stats_df
+
+
+def get_team_multiple(date_yyyymmdd):
+    teams_stat = get_team_stat()
+    teams_str = 'Golden State Warriors|Portland Trail Blazers|Houston Rockets|Los Angeles Lakers|Los Angeles Clippers|Toronto Raptors|Indiana Pacers|Charlotte Hornets|Denver Nuggets|New Orleans Pelicans|Phoenix Suns|Detroit Pistons|Oklahoma City Thunder|New York Knicks|Atlanta Hawks|Cleveland Cavaliers|Orlando Magic|Utah Jazz|Memphis Grizzlies|Boston Celtics|Minnesota Timberwolves|San Antonio Spurs|Brooklyn Nets|Sacramento Kings|Miami Heat|Chicago Bulls|Washington Wizards|Milwaukee Bucks|Philadelphia 76ers|Dallas Mavericks'
+    teams_regex = '({})({})'.format(teams_str, teams_str)
+    odd_url = 'http://www.donbest.com/nba/odds/{}.html'.format(date_yyyymmdd)
+    odd_df = pd.read_html(odd_url)[0]
+    odd_df.columns = odd_df.iloc[1]
+    odd_df = odd_df.reindex(odd_df.index.drop(0))
+    odd_df = odd_df.reindex(odd_df.index.drop(1))
+
+    team_mult = {}
+
+    for i, r in odd_df.iterrows():
+        teams = 1
+
+    # TODO: PK, -/+ in front
+
+    return
+
+    # get_team_multiple('20161216')
